f5bdc2cc06977207fdc8e069acb22872bfb1debe
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-02-24 10:09:59 +0100

    spark wordcount

---
+++ hadoop/spark/wordcount.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def split_words(line):
+    return line.split()
+
+def create_pair(word):
+    return (word, 1)
+
+text_RDD = sc.textFile("file:///home/cloudera/testfile1") # from local filesystem
+pairs_RDD = text_RDD.flatMap(split_words).map(create_pair)
+
+def sum_counts(a, b):
+    return a + b
+
+wordcounts_RDD = pairs_RDD.reduceByKey(sum_counts)
+
+wordcounts_RDD.collect()
+
