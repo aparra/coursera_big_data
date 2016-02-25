ec46c2513363a5c76d13125227e543f5468fb4d6
Anderson Parra de Paula <anderson.parradepaula@soundcloud.com>
2016-02-25 13:59:00 +0100

    spark join

---
+++ hadoop/spark/join.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+def split_fileA(line):
+    word, count = map(lambda e: e.strip(), line.strip().split(',')
+    return (word, int(count))
+
+def split_fileB(line):
+    data, count = map(lambda e: e.strip(), line.strip().split(',')
+    date, word = data.split(' ')
+    return (word, date + " " + count)
+ 
+fileA = sc.textFile("input/join1_FileA.txt")    
+fileA_data = fileA.map(split_fileA)
+
+fileB = sc.textFile("input/join1_FileV.txt")
+fileB_data = fileB.map(split_fileB)
+
+fileB_joined_FileA = fileB_data.join(fileA_data)
+
+print fileB_joined_FileA.collect()
